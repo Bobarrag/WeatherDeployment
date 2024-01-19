460c480bbbf69499e52c3a9dd10202374982c3fa
Bobarrag <45056614+Bobarrag@users.noreply.github.com>
2024-01-19 17:02:35 -0500

    Added fetch request to Google Geocode API for coordinates of zip code user entered

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,27 @@ const axios = require('axios');
 const app = express();
 
 app.use(express.json());
+app.use(express.json());
 
 app.get('/', async function(req, res) {
 	res.send((process.env.FOO));
 });
+
+app.post('/:zipcode', (req, res) => {
+	const{zipcode} = req.params
+	if (!zipcode) {
+		return res.status(400).send({status:'failed'})
+	}
+	getWeatherData(zipcode)
+})
+
+async function getWeatherData(zipCode) {
+	const apiKey = process.env.GOOGLE_MAPS_API_KEY
+	const res = await axios.get(
+		'https://maps.googleapis.com/maps/api/geocode/json?address='+zipCode+'&key='+ apiKey
+	)
+
+	console.log(JSON.stringify(res.data.results))
+}
 
 app.listen(process.env.PORT || 3001);
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import axios from 'axios';
 import './index.css'
 
 function App() {
 
+  const baseUrl = 'http://localhost:3001/'
   const[zipcode, setZipcode] = useState('')
+  const[coords, setCoords] = useState('')
 
+    const postZip = async() => {
+      try {
+        await axios.post(baseUrl + zipcode)
+      } catch (error) {
+        console.error('Error getting weather data: ', error)
+      }
+    }
   return (
     <>
       <form>
@@ -12,7 +22,7 @@ function App() {
           Please enter a zip code:
           <input name={zipcode} onChange={e=>setZipcode(e.target.value)} />
         </label>
-        <button type="submit">Submit</button> 
+        <button onClick={postZip}>Submit</button> 
         <p>
           {zipcode}
         </p>
