68b20f1a8397482cb19044a45bf3a77c1e90d18a
BoBarrag <bonnybarragan@bonnys-air.lan>
2024-01-22 13:40:23 -0500

    Added desktop styling to weather input

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
 
     const postZip = async(e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
+
       try {
         const res = await axios.post(baseUrl + zipcode)
         console.log(res)
@@ -29,18 +30,27 @@ function App() {
     }
   return (
     <>
-      <form onSubmit={(e) =>postZip(e)}>
-        <label>
-          Please enter a zip code:
-          <input name='zipcode' value={zipcode} onChange={(e)=>setZipcode(e.target.value)} />
-        </label>
-        <button type='submit'>Submit</button> 
-        {weather && (        
-        <p>
-          It is currently {weather.data.current.temperature_2m}° Fahrenheit
-        </p>)}
+      <div className={"bg-gray-800 h-24"}>
 
-      </form>
+      </div>
+      <div className = {"flex h-auto"}>
+        <form className = {"ml-24 mt-16 w-96 flex flex-col gap-12"} onSubmit={(e) =>postZip(e)}>
+          <label className= {"text-xl text-white text-6xl leading-snug font-semibold"}>
+            Please enter a 5 digit zip code:
+            
+          </label>
+          <input className = {"h-8"}name='zipcode' value={zipcode} onChange={(e)=>setZipcode(e.target.value)} />
+          <button className={`bg-blue-500 h-16 w-36 m-auto text-white px-4 py-2 rounded ${zipcode.length < 5 ? 'opacity-50 cursor-not-allowed' : ''}`}
+            disabled={zipcode.length < 5}>
+              Get Weather
+          </button> 
+          {weather && (        
+          <p>
+            It is currently {weather.data.current.temperature_2m}° Fahrenheit
+          </p>)}
+
+        </form>
+      </div>
     </>
   )
 }
